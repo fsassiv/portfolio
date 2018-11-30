c3f79e22aa8324de72e23fa1e2009753aec95693
Flavio <flavio.ssiv@gmail.com>
2018-11-29 21:25:03 -0300

    Sprites settings

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,9 @@ var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var webpack = require('webpack');
 var plumber = require('gulp-plumber');
+var svgSprite = require('gulp-svg-sprite');
+var del = require('del');
+var spritesmith = require('gulp.spritesmith');
 
 //####################################### PATHS #######################################
 var PORT = 5639;
@@ -118,4 +121,86 @@ gulp.task('cssInject', ['minifyCSS'], function() {
 //------------------------------------ JS RELOAD
 gulp.task('scriptsReload', ['scripts'], function() {
     browserSync.reload();
+});
+
+//------------------------------------ SPRITE PNG, JPEG, JPG IMAGES
+gulp.task('spritePNG', function() {
+    var spriteData = gulp.src(DIR_PATH + '/dev/img/**/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: 'spritePNG.css'
+        }));
+    return spriteData.pipe(gulp.dest(DIR_PATH + '/dev/temp/img/sprite'));
+});
+
+gulp.task('spriteJPEG', function() {
+    var spriteData = gulp.src(DIR_PATH + '/dev/img/**/*.{jpeg,jpg}')
+        .pipe(spritesmith({
+            imgName: 'sprite.jpeg',
+            cssName: 'spriteJPEG.css'
+        }));
+    return spriteData.pipe(gulp.dest(DIR_PATH + '/dev/temp/img/sprite'));
+});
+
+gulp.task('spriteALL', ['spritePNG', 'spriteJPEG'], function() {
+    console.log('Image files sprited');
+});
+
+//------------------------------------ SPRITE SVG IMAGES
+var config = {
+    mode: {
+        css: {
+            sprite: 'sprite.svg',
+            render: {
+                css: {
+                    template: DIR_PATH + '/dev/template/sprite.css'
+                }
+            }
+        }
+    }
+}
+
+//---delete temp folders/files
+gulp.task('beginClean', function() {
+    return del([DIR_PATH + '/dev/temp/sprite', DIR_PATH + '/dev/img/sprites']);
+});
+
+//--create sprites
+gulp.task('createSprite', ['beginClean'], function() {
+    return gulp.src(DIR_PATH + '/dev/img/toSprite/**/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest(DIR_PATH + '/dev/temp/sprite/'));
+});
+
+//--copy svgs
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+    return gulp.src(DIR_PATH + '/dev/temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest(DIR_PATH + '/dist/img/sprites'));
+});
+//--copy css
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+    return gulp.src(DIR_PATH + '/dev/temp/sprite/css/*.css')
+        .pipe(rename('spriteSVG.css'))
+        .pipe(gulp.dest(DIR_PATH + '/dist/css'));
+});
+
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
+    return del(DIR_PATH + '/dev/temp/sprite');
+});
+
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
+
+//------------------------------------ COMPRESS IMAGES
+gulp.task('compress', function() {
+    console.log('Starting compress task...');
+    return gulp.src(DIR_PATH + '/dist/img')
+        .pipe(imagemin([
+            imagemin.gifsicle(),
+            imagemin.jpegtran(),
+            imagemin.optipng(),
+            imagemin.svgo(),
+            pngquant(),
+            jpegrecompress()
+        ]))
+        .pipe(gulp.dest(DIST_PATH + '/dist/img'));
 });
